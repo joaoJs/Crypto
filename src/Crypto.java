cfdedf14790bb8bcf286021ea3269283b3294754
Joao Campos <j.campos4g@gmail.com>
2017-10-24 10:41:06 -0400

    method caesarify() added

--- src/Crypto.java
+++ src/Crypto.java
@@ -1,8 +1,11 @@
 public class Crypto {
     public static void main(String[] args) {
-        String original = "Hel^l!!o Wo r ld";
+        String original = "aHel^l!!o Wo r ld";
         original = normalizeText(original);
-        System.out.print(original);
+        System.out.println(original);
+        int key = -1;
+        original = caesarify(original, key);
+        System.out.println(original);
     }
 
     public static String normalizeText(String str) {
@@ -13,4 +16,22 @@ public class Crypto {
         str = str.replaceAll(pattern, "");
         return str;
     }
+
+    public static String caesarify(String str, int key) {
+        System.out.println((int)str.charAt(0));
+        System.out.println((char)72);
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            int j = (int) str.charAt(i);
+            int k = j + key;
+            if ((k - 65) > 25) {
+                k = ((k - 65 ) % 26) + 65;
+            } else if (k < 65) {
+                k = 91 - (65 - k);
+            }
+            res += (char)k;
+        }
+        return res;
+    }
+
 }
